9179a5237004bbdeb3c06ce085ee5fa9f5f02c8c
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-23 14:49:37 +0100

    setup, intial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import SignInScreen from './src/screens/SignInScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.root}>
+      <SignInScreen />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
+
+export default App;
