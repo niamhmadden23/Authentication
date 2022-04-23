40d9030b3a162ee6b64cc22a2bd50095deef81b8
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-23 15:16:24 +0100

    sign in screen added with custom input

--- App.js
+++ App.js
@@ -21,6 +21,7 @@ const App = () => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+    backgroundColor: 'red',
   },
 });
 
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import CustomInput from '../components/CustomInput';
 
 const SignInScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <View>
-      <Text>Sign in</Text>
+    <View style={styles.root}>
+      <CustomInput
+        placeholder="Username"
+        value={username}
+        setValue={setUsername}
+      />
+      <CustomInput
+        placeholder="Password"
+        value={password}
+        setValue={setPassword}
+        secureTextEntry
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: 'center',
+    paddingTop: 50,
+  },
+});
 
 export default SignInScreen;
