620c5653f49dbb6ff533d833e561f42064bcf2a3
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-26 10:23:44 +0100

    imported amplify theme to override default theming

--- App.js
+++ App.js
@@ -15,12 +15,14 @@ import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
 import NewPasswordScreen from './src/screens/NewPasswordScreen/NewPasswordScreen';
 import HomeScreen from './src/screens/HomeScreen/HomeScreen';
 import Navigation from './src/navigation';
-import Amplify from 'aws-amplify';
+import Amplify, {Auth} from 'aws-amplify';
 import config from './src/aws-exports';
+import {withAuthenticator, AmplifyTheme} from 'aws-amplify-react-native';
 
 Amplify.configure(config);
 
 const App = () => {
+  // Auth.signOut();
   return (
     <SafeAreaView style={styles.root}>
       {/* <SignUpScreen /> */}
@@ -39,4 +41,41 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+const signUpConfig = {
+  header: 'My Customized Sign Up',
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Full name',
+      key: 'name',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 2,
+      type: 'string',
+    },
+    {
+      label: 'Username',
+      key: 'preferred_username',
+      required: true,
+      displayOrder: 3,
+      type: 'string',
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 4,
+      type: 'password',
+    },
+  ],
+};
+
+const customThemes = {...AmplifyTheme};
+
+export default withAuthenticator(App, {signUpConfig, theme: customThemes});
